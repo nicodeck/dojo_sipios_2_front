27fbe7da347e6dfed2539f6449b8e31747e34511
Nicolas Descamps <nicolas.descamps@theodo.com>
2024-10-21 16:29:04 +0200

    chore: update Angular compiler-cli version

--- dojo-cs-cemantics/src/app/service/api-client.service.ts
+++ dojo-cs-cemantics/src/app/service/api-client.service.ts
@@ -1,16 +1,18 @@
-import {inject, Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {ResponseWord} from "../model/responseWord.model";
-import {GuessWord} from "../model/guessWord.model";
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ResponseWord } from '../model/responseWord.model';
+import { GuessWord } from '../model/guessWord.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiClientService {
   private httpClient = inject(HttpClient);
 
-  public getPokemonValue(guess: GuessWord): Observable<ResponseWord>{
-    return this.httpClient.get<ResponseWord>(`https://pokeapi.co/api/v2/pokemon/${guess}`)
+  public getPokemonValue(guess: GuessWord): Observable<ResponseWord> {
+    return this.httpClient.get<ResponseWord>(
+      `https://nicolasdesc-dojo-sipios-12.deno.dev/?guess=${guess.value}`
+    );
   }
 }
